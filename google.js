11e76bb88439c9641b78b695585530d209f79119
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-01-11 21:20:36 +0100

    Google, faute de mieux

---
+++ google.js
@@ -0,0 +1,4 @@
+document.getElementbyId("baguette").onclick = function() {
+document.getElementById("centre").style.display = "block";
+document.getElementbyId("baguette").style.display = "none";
+}
